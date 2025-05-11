a6565a7771ddbeb33fdbc6383758b6f18564d666
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-05-11 23:24:01 +0530

    fixes

--- code/src/components/Header.jsx
+++ code/src/components/Header.jsx
@@ -14,42 +14,42 @@ const Header = () => {
     {
       title: "Home",
       link: "/",
-      icon: <FontAwesomeIcon icon={faHome} />,
+      icon: <FontAwesomeIcon className= "text-sm md:text-lg"  icon={faHome} />,
     },
     {
       title: "Listings",
       link: "/listing",
-      icon: <FontAwesomeIcon icon={faList} />,
+      icon: <FontAwesomeIcon className= "text-sm md:text-lg" icon={faList} />,
     },
     {
       title: "Agents List",
       link: "/agents",
-      icon: <FontAwesomeIcon icon={faPersonDotsFromLine} />,
+      icon: <FontAwesomeIcon className= "text-sm md:text-lg" icon={faPersonDotsFromLine} />,
     },
     {
       title: "About Us",
       link: "/about-us",
-      icon: <FontAwesomeIcon icon={faInfoCircle} />,
+      icon: <FontAwesomeIcon className= "text-sm md:text-lg" icon={faInfoCircle} />,
     },
     {
       title: "Contact Us",
       link: "/contact-us",
-      icon: <FontAwesomeIcon icon={faContactBook} />,
+      icon: <FontAwesomeIcon className= "text-sm md:text-lg" icon={faContactBook} />,
     },
   ];
 
   return (
     <header className="fixed top-0 left-0 w-full shadow-md z-50">
       <div className="flex items-center justify-between flex-col bg-white md:bg-transparent md:flex-row min-h-15 text-white px-0 md:px-5">
-        <div className="max-w-25 md:h-20 w-full m-1">
-          <img src={logo} className="bg-white/60 rounded-full" alt="logo" />
+        <div className="max-w-25 h-20 w-full mb-2">
+          <img src={logo} className="bg-white/60 rounded-full  object-cover" alt="logo" />
         </div>
-        <nav className="flex overflow-x-auto gap-2 max-w-screen-lg md:max-w-screen-md md:justify-end w-full mb-2">
+        <nav className="flex overflow-x-auto scroll-hide gap-2 max-w-screen-lg md:max-w-screen-md md:justify-end w-full mb-2">
           {tabs.map((tab) => (
             <Link
               key={tab.title}
               to={tab.link}
-              className="flex items-center text-lg md:bg-blue-500/70 flex-shrink-0 px-4 py-2 rounded-2xl text-white bg-blue-900  hover:bg-blue-600 transition duration-300 ease-in-out whitespace-nowrap"
+              className="flex items-center text-sm md:text-lg md:bg-blue-500/70 flex-shrink-0 px-2 md:px-4 py-1 md:py-2 rounded-2xl text-white bg-blue-900  hover:bg-blue-600 transition duration-300 ease-in-out whitespace-nowrap"
             >
               {tab.icon}
               <span className="ml-2 w-full">{tab.title}</span>
